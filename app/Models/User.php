2ddf01c85a0ff44f396df5fc2f30446e044151fb
amortentiia <nashifazulfai@gmail.com>
2026-06-03 19:36:52 +0700

    tambah user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,31 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    // Fields yang bisa diisi massal
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'role'
-    ];
+    protected $fillable = ['name','email','password','role'];
 
-    // Fields yang disembunyikan saat serialisasi
-    protected $hidden = [
-        'password',
-        'remember_token'
-    ];
+    protected $hidden = ['password','remember_token'];
 
-    // Casting fields
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'email_verified_at'=>'datetime',
     ];
 }
