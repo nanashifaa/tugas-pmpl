9d997e2ff4233cbde1208dac9e21a528fb59839b
amortentiia <nashifazulfai@gmail.com>
2026-06-03 19:35:16 +0700

    tambah login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,20 +1,23 @@
+<?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function showLoginForm(){
+    public function showLoginForm() {
         return view('auth.login');
     }
 
-    public function login(Request $request){
+    public function login(Request $request) {
         $credentials = $request->validate([
             'email'=>'required|email',
             'password'=>'required'
         ]);
 
-        if(Auth::attempt($credentials)){
+        if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
             return redirect()->intended('/dashboard');
         }
